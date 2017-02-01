f0b87e2bb974df9ffebe972930454f9a5275b7a0
Pierre Ståhl <pierre.staahl@gmail.com>
2017-02-01 12:43:51 +0100

    0.0.1 Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyatv',
-    version='0.0.1.dev1',
+    version='0.0.1',
     license='MIT',
     url='https://github.com/postlund/pyatv',
     author='Pierre Ståhl',
