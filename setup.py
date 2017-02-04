9e24791ecf58eaa9125d48295b131e114dcfe511
Pierre Ståhl <pierre.staahl@gmail.com>
2017-02-04 19:33:49 +0100

    Verify pyatv with python 3.6 in tox

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries',
         'Topic :: Home Automation',
     ],
