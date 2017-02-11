385b280ccad9385d24d2ad3f892718c8302f8718
Pierre Ståhl <pierre.staahl@gmail.com>
2017-02-11 14:11:59 +0100

    Bump to next dev 0.1.5.dev1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyatv',
-    version='0.1.4',
+    version='0.1.5.dev1',
     license='MIT',
     url='https://github.com/postlund/pyatv',
     author='Pierre Ståhl',
