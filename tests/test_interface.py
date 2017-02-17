abfd7569917dea0ed5f45dffd5104ebf6fae4fe1
Pierre Ståhl <pierre.staahl@gmail.com>
2017-02-17 08:25:17 +0100

    Tests for playing output

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from pyatv import interface
+from pyatv import (const, convert, interface)
 
 
 class TestClass:
@@ -39,11 +39,62 @@ class TestClass:
         pass
 
 
+class PlayingDummy(interface.Playing):
+
+    def __init__(self, media_type=const.MEDIA_TYPE_VIDEO,
+                 play_state=const.PLAY_STATE_PLAYING, title=None, artist=None,
+                 album=None, total_time=None, position=None):
+        self._media_type = media_type
+        self._play_state = play_state
+        self._title = title
+        self._artist = artist
+        self._album = album
+        self._total_time = total_time
+        self._position = position
+
+    @property
+    def media_type(self):
+        """What type of media is currently playing, e.g. video, music."""
+        return self._media_type
+
+    @property
+    def play_state(self):
+        """Current play state, e.g. playing or paused."""
+        return self._play_state
+
+    @property
+    def title(self):
+        """Title of the current media, e.g. movie or song name."""
+        return self._title
+
+    @property
+    def artist(self):
+        """Artist of the currently playing song."""
+        return self._artist
+
+    @property
+    def album(self):
+        """Album of the currently playing song."""
+        return self._album
+
+    @property
+    def total_time(self):
+        """Total play time in seconds."""
+        return self._total_time
+
+    @property
+    def position(self):
+        """Current position in the playing media (seconds)."""
+        return self._position
+
+
 class InterfaceTest(unittest.TestCase):
 
     def setUp(self):
         self.obj = TestClass()
         self.methods = interface.retrieve_commands(self.obj)
+
+    # COMMANDS AND HELP TEXT
 
     def test_get_commands(self):
         self.assertEqual(5, len(self.methods))
@@ -68,3 +119,27 @@ class InterfaceTest(unittest.TestCase):
             'Type, e.g. a, b or c', self.methods['abbrev_help'])
         self.assertEqual(
             'Type, e.g. a, b or c', self.methods['abbrev_help_more_text'])
+
+    # PLAYING STR
+
+    def test_playing_media_type_and_playstate(self):
+        out = str(PlayingDummy())
+        self.assertIn(convert.media_type_str(const.MEDIA_TYPE_VIDEO), out)
+        self.assertIn(convert.playstate_str(const.PLAY_STATE_PLAYING), out)
+
+    def test_playing_title_artist_album(self):
+        out = str(PlayingDummy(
+            title='mytitle', artist='myartist', album='myalbum'))
+        self.assertIn('mytitle', out)
+        self.assertIn('myartist', out)
+        self.assertIn('myalbum', out)
+
+    def test_playing_only_position(self):
+        self.assertIn('1234', str(PlayingDummy(position=1234)))
+
+    def test_playing_only_total_time(self):
+        self.assertIn('5678', str(PlayingDummy(total_time=5678)))
+
+    def test_playing_both_position_and_total_time(self):
+        out = str(PlayingDummy(position=1234, total_time=5678))
+        self.assertIn('1234/5678', out)
