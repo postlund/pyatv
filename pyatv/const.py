5d45562d181345b4ab39e8b543d280227a1c9df3
Pierre Ståhl <pierre.staahl@gmail.com>
2017-06-19 07:26:36 +0200

    Release 0.3.0

--- pyatv/const.py
+++ pyatv/const.py
@@ -1,8 +1,8 @@
 """Constants used in the public API."""
 
 MAJOR_VERSION = 0
-MINOR_VERSION = 2
-PATCH_VERSION = '3.dev1'
+MINOR_VERSION = 3
+PATCH_VERSION = '0'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 
