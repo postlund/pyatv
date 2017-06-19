a89ce0ce0214c52f999e60d220faba3378be1d73
Pierre Ståhl <pierre.staahl@gmail.com>
2017-06-19 07:38:58 +0200

    Bump to next dev 0.3.1.dev1

--- pyatv/const.py
+++ pyatv/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 3
-PATCH_VERSION = '0'
+PATCH_VERSION = '1.dev1'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 
