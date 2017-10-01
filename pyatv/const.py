599b85a1487741d5ee80eb0a2bec142f077846eb
Pierre Ståhl <pierre.staahl@gmail.com>
2017-10-01 17:09:04 +0200

    Release 0.3.6

--- pyatv/const.py
+++ pyatv/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 3
-PATCH_VERSION = 5
+PATCH_VERSION = 6
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 
