acce9a50d4ef5cb9e62693a2311e8da47fa5b511
Pierre Ståhl <pierre.staahl@gmail.com>
2017-11-17 20:43:47 +0100

    Revert "Update airplay play_state logic"
    This reverts commit d6cd43d6c6bdc60857de11b8b9748c4794d6b4f3.

--- pyatv/airplay/player.py
+++ pyatv/airplay/player.py
@@ -6,8 +6,6 @@ import plistlib
 
 from aiohttp import ClientSession
 
-from pyatv import const
-
 _LOGGER = logging.getLogger(__name__)
 
 # This is the default port. It is also included in the Bonjour service
@@ -57,11 +55,13 @@ class AirPlayPlayer:
     def _url(self, port, command):
         return 'http://{}:{}/{}'.format(self.address, port, command)
 
-    # Poll playback-info to find out if something is playing.
+    # Poll playback-info to find out if something is playing. It might take
+    # some time until the media starts playing, give it 5 seconds (attempts)
     @asyncio.coroutine
     def _wait_for_media_to_end(self, session):
         address = self._url(self.port, 'playback-info')
-        play_state = const.PLAY_STATE_LOADING
+        attempts = 5
+        video_started = False
         while True:
             info = None
             try:
@@ -69,16 +69,18 @@ class AirPlayPlayer:
                 data = yield from info.content.read()
                 parsed = plistlib.loads(data)
 
-                if play_state == const.PLAY_STATE_LOADING:
-                    if 'duration' in parsed:
-                        play_state = const.PLAY_STATE_PLAYING
-                    elif 'readyToPlay' not in parsed:
-                        play_state = const.PLAY_STATE_NO_MEDIA
-                        break
-                elif play_state == const.PLAY_STATE_PLAYING:
-                    if 'duration' not in parsed:
-                        play_state = const.PLAY_STATE_NO_MEDIA
-                        break
+                # duration is only available if something is playing
+                if 'duration' in parsed:
+                    video_started = True
+                    attempts = -1
+                else:
+                    video_started = False
+                    if attempts >= 0:
+                        attempts -= 1
+
+                if not video_started and attempts < 0:
+                    _LOGGER.debug('media playback ended')
+                    break
 
             finally:
                 if info is not None:
