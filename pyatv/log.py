a7cc4314d3c655188a178e946237d93efbc79c22
Pierre Ståhl <pierre.staahl@gmail.com>
2018-02-20 23:16:13 +0100

    Use a determined order in log_binary

--- pyatv/log.py
+++ pyatv/log.py
@@ -9,5 +9,5 @@ def log_binary(logger, message, **kwargs):
     """Log binary data if debug is enabled."""
     if logger.isEnabledFor(logging.DEBUG):
         output = ('{0}={1}'.format(k, binascii.hexlify(
-            bytearray(v)).decode()) for k, v in kwargs.items())
+            bytearray(v)).decode()) for k, v in sorted(kwargs.items()))
         logger.debug('%s (%s)', message, ', '.join(output))
