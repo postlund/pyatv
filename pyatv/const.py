b240331e9fc31236bdc39059c654593f0a1e6c40
Pierre Ståhl <pierre.staahl@gmail.com>
2019-08-26 06:18:32 +0200

    Relase 0.3.13

--- pyatv/const.py
+++ pyatv/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 3
-PATCH_VERSION = 12
+PATCH_VERSION = 13
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 
