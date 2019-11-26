c82cfa2847bb8264ef4df45be89fceaba168032c
Pierre Ståhl <pierre.staahl@gmail.com>
2019-11-26 07:00:48 +0100

    Fifth pre-prelease of 0.4.0

--- pyatv/const.py
+++ pyatv/const.py
@@ -2,7 +2,7 @@
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 4
-PATCH_VERSION = '0a4'
+PATCH_VERSION = '0a5'
 __short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
 __version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
 
