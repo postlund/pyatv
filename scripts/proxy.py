830448f8674e789e103a95208e8461663d100b91
Pierre Ståhl <pierre.staahl@gmail.com>
2020-01-23 22:02:07 +0100

    Extract SRP server code in proxy
    All code related to the SRP server has been extracted into its own class
    (plus some helper functions). It will soon move into the fake MRP device
    so that MRP tests can be written for authentication and pairing.

--- scripts/proxy.py
+++ scripts/proxy.py
@@ -50,6 +50,7 @@ import hashlib
 import asyncio
 import logging
 import binascii
+from collections import namedtuple
 
 from aiozeroconf import Zeroconf, ServiceInfo
 
@@ -66,6 +67,8 @@ from pyatv.log import log_binary
 
 _LOGGER = logging.getLogger(__name__)
 
+ServerKeys = namedtuple('ServerKeys', 'sign auth auth_pub verify verify_pub')
+
 DEVICE_NAME = 'Proxy'
 IDENTIFIER = '5D797FD3-3538-427E-A47B-A32FC6CF3A69'
 AIRPLAY_IDENTIFIER = '4D797FD3-3538-427E-A47B-A32FC6CF3A6A'
@@ -81,64 +84,215 @@ def seed():
     return a
 
 
-# TODO: Refactor and clean up: this is very messy
-class ProxyMrpAppleTV(asyncio.Protocol):  # pylint: disable=too-many-instance-attributes  # noqa
+def generate_keys(seed):
+    """Generate server encryption keys from seed."""
+    signing_key = SigningKey(seed)
+    verify_private = curve25519.Private(secret=seed)
+    return ServerKeys(
+        sign=signing_key,
+        auth=signing_key.to_bytes(),
+        auth_pub=signing_key.get_verifying_key().to_bytes(),
+        verify=verify_private,
+        verify_pub=verify_private.get_public())
+
+
+def new_server_session(keys, pin):
+    """Create SRP server session."""
+    context = SRPContext(
+        'Pair-Setup', str(pin),
+        prime=constants.PRIME_3072,
+        generator=constants.PRIME_3072_GEN,
+        hash_func=hashlib.sha512,
+        bits_salt=128)
+    username, verifier, salt = context.get_user_data_triplet()
+
+    context_server = SRPContext(
+        username,
+        prime=constants.PRIME_3072,
+        generator=constants.PRIME_3072_GEN,
+        hash_func=hashlib.sha512,
+        bits_salt=128)
+
+    session = SRPServerSession(
+        context_server,
+        verifier,
+        binascii.hexlify(keys.auth).decode())
+
+    return session, salt
+
+
+class ServerAuth:
+    """Server-side implementation of MRP authentication."""
+
+    def __init__(self, delegate, unique_id):
+        """Initialize a new instance if ServerAuth."""
+        self.delegate = delegate
+        self.unique_id = unique_id
+        self.input_key = None
+        self.output_key = None
+        self.has_paired = False
+        self.keys = generate_keys(seed())
+        self.session, self.salt = new_server_session(self.keys, 1111)
+
+    def handle_device_info(self, message, _):
+        """Handle received device information message."""
+        _LOGGER.debug('Received device info message')
+
+        # TODO: Consolidate this better with messages.device_information(...)
+        resp = messages.create(
+            protobuf.DEVICE_INFO_MESSAGE, identifier=message.identifier)
+        resp.inner().uniqueIdentifier = self.unique_id
+        resp.inner().name = DEVICE_NAME
+        resp.inner().localizedModelName = DEVICE_NAME
+        resp.inner().systemBuildVersion = '17K449'
+        resp.inner().applicationBundleIdentifier = 'com.apple.mediaremoted'
+        resp.inner().protocolVersion = 1
+        resp.inner().lastSupportedMessageType = 77
+        resp.inner().supportsSystemPairing = True
+        resp.inner().allowsPairing = True
+        resp.inner().systemMediaApplication = "com.apple.TVMusic"
+        resp.inner().supportsACL = True
+        resp.inner().supportsSharedQueue = True
+        resp.inner().supportsExtendedMotion = True
+        resp.inner().sharedQueueVersion = 2
+        resp.inner().deviceClass = 4
+        self.delegate.send(resp)
+
+    def handle_crypto_pairing(self, message, inner):
+        """Handle incoming crypto pairing message."""
+        _LOGGER.debug('Received crypto pairing message')
+        pairing_data = tlv8.read_tlv(inner.pairingData)
+        seqno = pairing_data[tlv8.TLV_SEQ_NO][0]
+
+        # Work-around for now to support "tries" to auth before pairing
+        if seqno == 1:
+            if tlv8.TLV_PUBLIC_KEY in pairing_data:
+                self.has_paired = True
+            elif tlv8.TLV_METHOD in pairing_data:
+                self.has_paired = False
+
+        suffix = 'paired' if self.has_paired else 'pairing'
+        method = '_seqno{0}_{1}'.format(seqno, suffix)
+        getattr(self, method)(pairing_data)
+
+    def _seqno1_paired(self, pairing_data):
+        server_pub_key = self.keys.verify_pub.serialize()
+        client_pub_key = pairing_data[tlv8.TLV_PUBLIC_KEY]
+
+        shared_key = self.keys.verify.get_shared_key(
+            curve25519.Public(client_pub_key), hashfunc=lambda x: x)
+
+        session_key = hkdf_expand('Pair-Verify-Encrypt-Salt',
+                                  'Pair-Verify-Encrypt-Info',
+                                  shared_key)
+
+        info = server_pub_key + self.unique_id + client_pub_key
+        signature = self.keys.sign.sign(info)
+
+        tlv = tlv8.write_tlv({
+            tlv8.TLV_IDENTIFIER: self.unique_id,
+            tlv8.TLV_SIGNATURE: signature
+        })
+
+        chacha = chacha20.Chacha20Cipher(session_key, session_key)
+        encrypted = chacha.encrypt(tlv, nounce='PV-Msg02'.encode())
+
+        msg = messages.crypto_pairing({
+            tlv8.TLV_SEQ_NO: b'\x02',
+            tlv8.TLV_PUBLIC_KEY: server_pub_key,
+            tlv8.TLV_ENCRYPTED_DATA: encrypted
+        })
+
+        self.output_key = hkdf_expand('MediaRemote-Salt',
+                                      'MediaRemote-Write-Encryption-Key',
+                                      shared_key)
+
+        self.input_key = hkdf_expand('MediaRemote-Salt',
+                                     'MediaRemote-Read-Encryption-Key',
+                                     shared_key)
+
+        log_binary(_LOGGER,
+                   'Keys',
+                   Output=self.output_key,
+                   Input=self.input_key)
+        self.delegate.send(msg)
+
+    def _seqno1_pairing(self, pairing_data):
+        msg = messages.crypto_pairing({
+            tlv8.TLV_SALT: binascii.unhexlify(self.salt),
+            tlv8.TLV_PUBLIC_KEY: binascii.unhexlify(self.session.public),
+            tlv8.TLV_SEQ_NO: b'\x02'
+        })
+
+        self.delegate.send(msg)
+
+    def _seqno3_paired(self, pairing_data):
+        self.delegate.send(messages.crypto_pairing({tlv8.TLV_SEQ_NO: b'\x04'}))
+        self.delegate.enable_encryption(self.input_key, self.output_key)
+
+    def _seqno3_pairing(self, pairing_data):
+        pubkey = binascii.hexlify(
+            pairing_data[tlv8.TLV_PUBLIC_KEY]).decode()
+        self.session.process(pubkey, self.salt)
+
+        proof = binascii.unhexlify(self.session.key_proof_hash)
+        assert self.session.verify_proof(
+            binascii.hexlify(pairing_data[tlv8.TLV_PROOF]))
+
+        msg = messages.crypto_pairing({
+            tlv8.TLV_PROOF: proof,
+            tlv8.TLV_SEQ_NO: b'\x04'
+        })
+        self.delegate.send(msg)
+
+    def _seqno5_pairing(self, _):
+        session_key = hkdf_expand(
+            'Pair-Setup-Encrypt-Salt',
+            'Pair-Setup-Encrypt-Info',
+            binascii.unhexlify(self.session.key))
+
+        acc_device_x = hkdf_expand(
+            'Pair-Setup-Accessory-Sign-Salt',
+            'Pair-Setup-Accessory-Sign-Info',
+            binascii.unhexlify(self.session.key))
+
+        device_info = acc_device_x + self.unique_id + self.keys.auth_pub
+        signature = self.keys.sign.sign(device_info)
+
+        tlv = tlv8.write_tlv({tlv8.TLV_IDENTIFIER: self.unique_id,
+                              tlv8.TLV_PUBLIC_KEY: self.keys.auth_pub,
+                              tlv8.TLV_SIGNATURE: signature})
+
+        chacha = chacha20.Chacha20Cipher(session_key, session_key)
+        encrypted = chacha.encrypt(tlv, nounce='PS-Msg06'.encode())
+
+        msg = messages.crypto_pairing({
+            tlv8.TLV_SEQ_NO: b'\x06',
+            tlv8.TLV_ENCRYPTED_DATA: encrypted,
+        })
+        self.has_paired = True
+
+        self.delegate.send(msg)
+
+
+class ProxyMrpAppleTV(ServerAuth, asyncio.Protocol):
     """Implementation of a fake MRP Apple TV."""
 
-    def __init__(self, loop, credentials):
+    def __init__(self, loop, identifier=IDENTIFIER):
         """Initialize a new instance of ProxyMrpAppleTV."""
         self.loop = loop
-        self.credentials = credentials
-        self.atv_device_id = IDENTIFIER.encode()
+        self.auther = ServerAuth(self, identifier.encode())
         self.buffer = b''
-        self.has_paired = False
         self.transport = None
         self.chacha = None
         self.connection = None
-        self.input_key = None
-        self.output_key = None
-        self.mapping = {
-            protobuf.DEVICE_INFO_MESSAGE: self.handle_device_info,
-            protobuf.CRYPTO_PAIRING_MESSAGE: self.handle_crypto_pairing,
-            }
 
-        self._shared = None
-        self._session_key = None
-
-        self._signing_key = SigningKey(seed())
-        self._auth_private = self._signing_key.to_bytes()
-        self._auth_public = self._signing_key.get_verifying_key().to_bytes()
-        self._verify_private = curve25519.Private(secret=seed())
-        self._verify_public = self._verify_private.get_public()
-
-        self.context = SRPContext(
-            'Pair-Setup', str(1111),
-            prime=constants.PRIME_3072,
-            generator=constants.PRIME_3072_GEN,
-            hash_func=hashlib.sha512,
-            bits_salt=128)
-        self.username, self.verifier, self.salt = \
-            self.context.get_user_data_triplet()
-
-        context_server = SRPContext(
-            'Pair-Setup',
-            prime=constants.PRIME_3072,
-            generator=constants.PRIME_3072_GEN,
-            hash_func=hashlib.sha512,
-            bits_salt=128)
-
-        self._session = SRPServerSession(
-            context_server,
-            self.verifier,
-            binascii.hexlify(self._auth_private).decode())
-
-    async def start(self, address, port):
+    async def start(self, address, port, credentials):
         """Start the proxy instance."""
-        # Establish connection to ATV
         self.connection = MrpConnection(address, port, self.loop)
         protocol = MrpProtocol(
             self.loop, self.connection, SRPAuthHandler(),
-            MrpService(None, port, credentials=self.credentials))
+            MrpService(None, port, credentials=credentials))
         await protocol.start(skip_initial_messages=True)
         self.connection.listener = self
         self._process_buffer()
@@ -147,7 +301,13 @@ class ProxyMrpAppleTV(asyncio.Protocol):  # pylint: disable=too-many-instance-at
         """Client did connect to proxy."""
         self.transport = transport
 
-    def _send(self, message):
+    def enable_encryption(self, input_key, output_key):
+        """Enable encryption with specified keys."""
+        self.chacha = chacha20.Chacha20Cipher(
+            input_key, output_key)
+
+    def send(self, message):
+        """Send protobuf message to client."""
         data = message.SerializeToString()
         _LOGGER.info('<<(DECRYPTED): %s', message)
         if self.chacha:
@@ -157,7 +317,8 @@ class ProxyMrpAppleTV(asyncio.Protocol):  # pylint: disable=too-many-instance-at
         length = variant.write_variant(len(data))
         self.transport.write(length + data)
 
-    def _send_raw(self, raw):
+    def send_raw(self, raw):
+        """Send raw data to client."""
         parsed = protobuf.ProtocolMessage()
         parsed.ParseFromString(raw)
 
@@ -175,7 +336,7 @@ class ProxyMrpAppleTV(asyncio.Protocol):  # pylint: disable=too-many-instance-at
 
     def message_received(self, _, raw):
         """Message received from ATV."""
-        self._send_raw(raw)
+        self.send_raw(raw)
 
     def data_received(self, data):
         """Message received from iOS app/client."""
@@ -195,155 +356,19 @@ class ProxyMrpAppleTV(asyncio.Protocol):  # pylint: disable=too-many-instance-at
                 log_binary(_LOGGER, 'ENC Phone->ATV', Encrypted=data)
                 data = self.chacha.decrypt(data)
 
-            parsed = protobuf.ProtocolMessage()
-            parsed.ParseFromString(data)
-            _LOGGER.info('(DEC Phone->ATV): %s', parsed)
+            message = protobuf.ProtocolMessage()
+            message.ParseFromString(data)
+            _LOGGER.info('(DEC Phone->ATV): %s', message)
 
             try:
-                def unhandled_message(_, raw):
-                    self.connection.send_raw(raw)
-
-                self.mapping.get(parsed.type, unhandled_message)(parsed, data)
+                if message.type == protobuf.DEVICE_INFO_MESSAGE:
+                    self.auther.handle_device_info(message, message.inner())
+                elif message.type == protobuf.CRYPTO_PAIRING_MESSAGE:
+                    self.auther.handle_crypto_pairing(message, message.inner())
+                else:
+                    self.connection.send_raw(data)
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception('Error while dispatching message')
-
-    def handle_device_info(self, message, _):
-        """Handle received device information message."""
-        _LOGGER.debug('Received device info message')
-
-        # TODO: Consolidate this better with message.device_information(...)
-        resp = messages.create(protobuf.DEVICE_INFO_MESSAGE)
-        resp.identifier = message.identifier
-        resp.inner().uniqueIdentifier = self.atv_device_id
-        resp.inner().name = DEVICE_NAME
-        resp.inner().localizedModelName = DEVICE_NAME
-        resp.inner().systemBuildVersion = '17K449'
-        resp.inner().applicationBundleIdentifier = 'com.apple.mediaremoted'
-        resp.inner().protocolVersion = 1
-        resp.inner().lastSupportedMessageType = 77
-        resp.inner().supportsSystemPairing = True
-        resp.inner().allowsPairing = True
-        resp.inner().systemMediaApplication = "com.apple.TVMusic"
-        resp.inner().supportsACL = True
-        resp.inner().supportsSharedQueue = True
-        resp.inner().supportsExtendedMotion = True
-        resp.inner().sharedQueueVersion = 2
-        resp.inner().deviceClass = 4
-        self._send(resp)
-
-    def handle_crypto_pairing(self, message, _):
-        """Handle incoming crypto pairing message."""
-        _LOGGER.debug('Received crypto pairing message')
-        pairing_data = tlv8.read_tlv(message.inner().pairingData)
-        seqno = pairing_data[tlv8.TLV_SEQ_NO][0]
-
-        if seqno == 1:
-            if tlv8.TLV_PUBLIC_KEY in pairing_data:
-                self.has_paired = True
-            elif tlv8.TLV_METHOD in pairing_data:
-                self.has_paired = False
-
-        getattr(self, "_seqno_" + str(seqno))(pairing_data)
-
-    def _seqno_1(self, pairing_data):
-        if self.has_paired:
-            server_pub_key = self._verify_public.serialize()
-            client_pub_key = pairing_data[tlv8.TLV_PUBLIC_KEY]
-
-            self._shared = self._verify_private.get_shared_key(
-                curve25519.Public(client_pub_key), hashfunc=lambda x: x)
-
-            session_key = hkdf_expand('Pair-Verify-Encrypt-Salt',
-                                      'Pair-Verify-Encrypt-Info',
-                                      self._shared)
-
-            info = server_pub_key + self.atv_device_id + client_pub_key
-            signature = self._signing_key.sign(info)
-
-            tlv = tlv8.write_tlv({
-                tlv8.TLV_IDENTIFIER: self.atv_device_id,
-                tlv8.TLV_SIGNATURE: signature
-            })
-
-            chacha = chacha20.Chacha20Cipher(session_key, session_key)
-            encrypted = chacha.encrypt(tlv, nounce='PV-Msg02'.encode())
-
-            msg = messages.crypto_pairing({
-                tlv8.TLV_SEQ_NO: b'\x02',
-                tlv8.TLV_PUBLIC_KEY: server_pub_key,
-                tlv8.TLV_ENCRYPTED_DATA: encrypted
-            })
-
-            self.output_key = hkdf_expand('MediaRemote-Salt',
-                                          'MediaRemote-Write-Encryption-Key',
-                                          self._shared)
-
-            self.input_key = hkdf_expand('MediaRemote-Salt',
-                                         'MediaRemote-Read-Encryption-Key',
-                                         self._shared)
-
-            log_binary(_LOGGER,
-                       'Keys',
-                       Output=self.output_key,
-                       Input=self.input_key)
-
-        else:
-            msg = messages.crypto_pairing({
-                tlv8.TLV_SALT: binascii.unhexlify(self.salt),
-                tlv8.TLV_PUBLIC_KEY: binascii.unhexlify(self._session.public),
-                tlv8.TLV_SEQ_NO: b'\x02'
-            })
-
-        self._send(msg)
-
-    def _seqno_3(self, pairing_data):
-        if self.has_paired:
-            self._send(messages.crypto_pairing({tlv8.TLV_SEQ_NO: b'\x04'}))
-            self.chacha = chacha20.Chacha20Cipher(
-                self.input_key, self.output_key)
-        else:
-            pubkey = binascii.hexlify(
-                pairing_data[tlv8.TLV_PUBLIC_KEY]).decode()
-            self._session.process(pubkey, self.salt)
-
-            proof = binascii.unhexlify(self._session.key_proof_hash)
-            assert self._session.verify_proof(
-                binascii.hexlify(pairing_data[tlv8.TLV_PROOF]))
-
-            msg = messages.crypto_pairing({
-                tlv8.TLV_PROOF: proof,
-                tlv8.TLV_SEQ_NO: b'\x04'
-            })
-            self._send(msg)
-
-    def _seqno_5(self, _):
-        self._session_key = hkdf_expand(
-            'Pair-Setup-Encrypt-Salt',
-            'Pair-Setup-Encrypt-Info',
-            binascii.unhexlify(self._session.key))
-
-        acc_device_x = hkdf_expand(
-            'Pair-Setup-Accessory-Sign-Salt',
-            'Pair-Setup-Accessory-Sign-Info',
-            binascii.unhexlify(self._session.key))
-
-        device_info = acc_device_x + self.atv_device_id + self._auth_public
-        signature = self._signing_key.sign(device_info)
-
-        tlv = tlv8.write_tlv({tlv8.TLV_IDENTIFIER: self.atv_device_id,
-                              tlv8.TLV_PUBLIC_KEY: self._auth_public,
-                              tlv8.TLV_SIGNATURE: signature})
-
-        chacha = chacha20.Chacha20Cipher(self._session_key, self._session_key)
-        encrypted = chacha.encrypt(tlv, nounce='PS-Msg06'.encode())
-
-        msg = messages.crypto_pairing({
-            tlv8.TLV_SEQ_NO: b'\x06',
-            tlv8.TLV_ENCRYPTED_DATA: encrypted,
-        })
-        self.has_paired = True
-
-        self._send(msg)
 
 
 async def publish_zeroconf(zconf, ip_address, port):
@@ -392,9 +417,10 @@ async def main(loop):
 
     def proxy_factory():
         try:
-            proxy = ProxyMrpAppleTV(loop, credentials)
+            proxy = ProxyMrpAppleTV(loop)
             asyncio.ensure_future(
-                proxy.start(atv_ip_addr, atv_port), loop=loop)
+                proxy.start(atv_ip_addr, atv_port, credentials),
+                loop=loop)
         except Exception:
             _LOGGER.exception("failed to start proxy")
         return proxy
