5f46dacccea8e107d0407c95432eda611980ef81
Pierre Ståhl <pierre.staahl@gmail.com>
2020-02-10 19:36:16 +0100

    Consolidate protobuf scripts into one script
    Fixes #235.

--- scripts/protobuf.py
+++ scripts/protobuf.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-#
-# This is a quick-hack that extends ProtocolMessage with a method called inner
-# that will return the correct submessage based on the type.
-#
-"""Simple hack to auto-generate protobuf handling code."""
+"""Simple tool to work with protobuf in pyatv."""
 
 import os
 import sys
 import glob
+import binascii
+import argparse
 import subprocess
 from collections import namedtuple
+
+import cryptography
+from google.protobuf.text_format import MessageToString
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+
+from pyatv.mrp import variant
+from pyatv.mrp.protobuf import ProtocolMessage_pb2 as PB
 
 
 # New messages re-using inner message of another type
@@ -139,8 +144,8 @@ def generate_module_code():
         )
 
 
-def main():
-    """Script starts here."""
+def update_auto_generated_code():
+    """Generate and update auto-generated wrapper code."""
     if not os.path.exists(".git"):
         print("Run this script from repo root", file=sys.stderr)
         return 1
@@ -156,6 +161,98 @@ def main():
         fh.write(module_code)
 
     return 0
+
+
+def _print_single_message(data, unknown_fields):
+    parsed = PB.ProtocolMessage()
+    parsed.ParseFromString(data)
+    output = MessageToString(
+        parsed, print_unknown_fields=unknown_fields)
+    print(output)
+
+
+def decode_and_print_message(args):
+    """Decode and print protobuf messages."""
+    buf = binascii.unhexlify(args.message)
+    if not args.stream:
+        buf = variant.write_variant(len(buf)) + buf
+
+    while buf:
+        length, raw = variant.read_variant(buf)
+        data = raw[:length]
+        buf = raw[length:]
+        _print_single_message(data, args.unknown_fields)
+
+    return 0
+
+
+def _decrypt_chacha20poly1305(data, nounce, key):
+    """Decrypt data with specified key and nounce."""
+    data = binascii.unhexlify(data)
+    input_key = binascii.unhexlify(key)
+    input_nonce = b'\x00\x00\x00\x00' + nounce.to_bytes(
+        length=8, byteorder='little')
+    chacha = ChaCha20Poly1305(input_key)
+    try:
+        print('Trying key {0} with nounce {1}'.format(input_key, input_nonce))
+        decrypted_data = chacha.decrypt(input_nonce, data, None)
+        print('Data decrypted!\n - Nounce : {0}'
+              '\n - Key    : {1}\n - Data   : {2}\n'.format(
+                binascii.hexlify(input_nonce).decode(),
+                binascii.hexlify(input_key).decode(),
+                binascii.hexlify(decrypted_data).decode()))
+        _print_single_message(decrypted_data, True)
+        return True
+    except cryptography.exceptions.InvalidTag:
+        pass
+
+    return False
+
+
+def decrypt_and_print_message(args):
+    """Try to decrypt and print a message."""
+    for key in args.keys:
+        for nounce in range(args.nounce_lower, args.nounce_upper):
+            if _decrypt_chacha20poly1305(args.message, nounce, key):
+                return 0
+    return 1
+
+
+def main():
+    """Script starts here."""
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help="sub commands", dest="command")
+
+    subparsers.add_parser("generate", help="generate protobuf wrapper")
+
+    decode = subparsers.add_parser("decode", help="decode protobuf message(s)")
+    decode.add_argument("message", help="message in hex to decode")
+    decode.add_argument("-u", "--unknown-fields", action="store_true",
+                        help="include unknown fields")
+    decode.add_argument("-s", "--stream", action="store_true",
+                        help="decode protocol stream of messages")
+
+    decrypt = subparsers.add_parser("decrypt", help="decrypt protobuf message")
+    decrypt.add_argument("message", help="message in hex to decrypt")
+    decrypt.add_argument("keys", nargs="+", help="keys to decrypt with")
+    decrypt.add_argument("-l", "--nounce-lower", type=int, default=0,
+                         help="start value for nounce")
+    decrypt.add_argument("-u", "--nounce-upper", type=int, default=128,
+                         help="upper value for nounce")
+
+    args = parser.parse_args()
+    if not args.command:
+        parser.error("No command specified")
+        return 1
+
+    if args.command == "generate":
+        return update_auto_generated_code()
+    elif args.command == "decode":
+        return decode_and_print_message(args)
+    elif args.command == "decrypt":
+        return decrypt_and_print_message(args)
+
+    return 1
 
 
 if __name__ == "__main__":
