23accc6245d857f1fb0db00489113ee5c45de32a
Pierre Ståhl <pierre.staahl@gmail.com>
2020-07-16 08:59:03 +0200

    gha: Run tests with python 3.9
    Install openssl on windows as cryptography does not provide wheels yet.
    
    Fixes #762.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 # Read in version without importing pyatv
 # http://stackoverflow.com/questions/6357361/alternative-to-execfile-in-python-3
-exec(compile(open('pyatv/const.py', "rb").read(), 'pyatv/const.py', 'exec'))
+exec(compile(open("pyatv/const.py", "rb").read(), "pyatv/const.py", "exec"))
 
 
 def read(fname):
@@ -21,40 +21,41 @@ def get_requirements():
 
 
 setup(
-    name='pyatv',
+    name="pyatv",
     version=__version__,
-    license='MIT',
-    url='https://github.com/postlund/pyatv',
-    author='Pierre Ståhl',
-    author_email='pierre.staahl@gmail.com',
-    description='Library for controlling an Apple TV',
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    license="MIT",
+    url="https://github.com/postlund/pyatv",
+    author="Pierre Ståhl",
+    author_email="pierre.staahl@gmail.com",
+    description="Library for controlling an Apple TV",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
+    platforms="any",
     install_requires=get_requirements(),
-    test_suite='tests',
-    keywords=['apple', 'tv'],
-    setup_requires=['pytest-runner'],
-    tests_require=['tox==3.17.1', 'pytest==5.4.3', 'pytest-xdist==1.33.0'],
+    test_suite="tests",
+    keywords=["apple", "tv"],
+    setup_requires=["pytest-runner"],
+    tests_require=["tox==3.17.1", "pytest==5.4.3", "pytest-xdist==1.33.0"],
     entry_points={
-        'console_scripts': [
-            'atvremote = pyatv.scripts.atvremote:main',
-            'atvproxy = pyatv.scripts.atvproxy:main',
-            'atvscript = pyatv.scripts.atvscript:main'
+        "console_scripts": [
+            "atvremote = pyatv.scripts.atvremote:main",
+            "atvproxy = pyatv.scripts.atvproxy:main",
+            "atvscript = pyatv.scripts.atvscript:main"
         ]
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Home Automation',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Home Automation",
     ],
 )
