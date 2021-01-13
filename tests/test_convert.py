aec7362144bc2d057fb331fe2ecfc9e806a6f3f7
Pierre Ståhl <pierre.staahl@gmail.com>
2021-01-13 13:04:52 +0100

    mrp: Add support for heartbeats (#926)
    Fixes #887

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,52 +1,58 @@
 """Unit tests for pyatv.convert."""
 
-import unittest
-
 from pyatv import convert
 from pyatv.const import Protocol, MediaType, DeviceState, RepeatState, ShuffleState
 
 
-class ConvertTest(unittest.TestCase):
-    def test_media_type_to_string(self):
-        self.assertEqual("Unknown", convert.media_type_str(MediaType.Unknown))
-        self.assertEqual("Video", convert.media_type_str(MediaType.Video))
-        self.assertEqual("Music", convert.media_type_str(MediaType.Music))
-        self.assertEqual("TV", convert.media_type_str(MediaType.TV))
+def test_media_type_to_string():
+    assert "Unknown" == convert.media_type_str(MediaType.Unknown)
+    assert "Video" == convert.media_type_str(MediaType.Video)
+    assert "Music" == convert.media_type_str(MediaType.Music)
+    assert "TV" == convert.media_type_str(MediaType.TV)
 
-    def test_unknown_media_type_to_str(self):
-        self.assertEqual("Unsupported", convert.media_type_str(999))
 
-    def test_device_state_str(self):
-        self.assertEqual("Idle", convert.device_state_str(DeviceState.Idle))
-        self.assertEqual("Loading", convert.device_state_str(DeviceState.Loading))
-        self.assertEqual("Stopped", convert.device_state_str(DeviceState.Stopped))
-        self.assertEqual("Paused", convert.device_state_str(DeviceState.Paused))
-        self.assertEqual("Playing", convert.device_state_str(DeviceState.Playing))
-        self.assertEqual("Seeking", convert.device_state_str(DeviceState.Seeking))
+def test_unknown_media_type_to_str():
+    assert "Unsupported" == convert.media_type_str(999)
 
-    def test_unsupported_device_state_str(self):
-        self.assertEqual("Unsupported", convert.device_state_str(999))
 
-    def test_repeat_str(self):
-        self.assertEqual("Off", convert.repeat_str(RepeatState.Off))
-        self.assertEqual("Track", convert.repeat_str(RepeatState.Track))
-        self.assertEqual("All", convert.repeat_str(RepeatState.All))
+def test_device_state_str():
+    assert "Idle" == convert.device_state_str(DeviceState.Idle)
+    assert "Loading" == convert.device_state_str(DeviceState.Loading)
+    assert "Stopped" == convert.device_state_str(DeviceState.Stopped)
+    assert "Paused" == convert.device_state_str(DeviceState.Paused)
+    assert "Playing" == convert.device_state_str(DeviceState.Playing)
+    assert "Seeking" == convert.device_state_str(DeviceState.Seeking)
 
-    def test_unknown_repeat_to_str(self):
-        self.assertEqual("Unsupported", convert.repeat_str(1234))
 
-    def test_shuffle_str(self):
-        self.assertEqual("Off", convert.shuffle_str(ShuffleState.Off))
-        self.assertEqual("Albums", convert.shuffle_str(ShuffleState.Albums))
-        self.assertEqual("Songs", convert.shuffle_str(ShuffleState.Songs))
+def test_unsupported_device_state_str():
+    assert "Unsupported" == convert.device_state_str(999)
 
-    def test_unknown_shuffle_to_str(self):
-        self.assertEqual("Unsupported", convert.shuffle_str(1234))
 
-    def test_protocol_str(self):
-        self.assertEqual("MRP", convert.protocol_str(Protocol.MRP))
-        self.assertEqual("DMAP", convert.protocol_str(Protocol.DMAP))
-        self.assertEqual("AirPlay", convert.protocol_str(Protocol.AirPlay))
+def test_repeat_str():
+    assert "Off" == convert.repeat_str(RepeatState.Off)
+    assert "Track" == convert.repeat_str(RepeatState.Track)
+    assert "All" == convert.repeat_str(RepeatState.All)
 
-    def test_unknown_protocol_str(self):
-        self.assertEqual("Unknown", convert.protocol_str("invalid"))
+
+def test_unknown_repeat_to_str():
+    assert "Unsupported" == convert.repeat_str(1234)
+
+
+def test_shuffle_str():
+    assert "Off" == convert.shuffle_str(ShuffleState.Off)
+    assert "Albums" == convert.shuffle_str(ShuffleState.Albums)
+    assert "Songs" == convert.shuffle_str(ShuffleState.Songs)
+
+
+def test_unknown_shuffle_to_str():
+    assert "Unsupported" == convert.shuffle_str(1234)
+
+
+def test_protocol_str():
+    assert "MRP" == convert.protocol_str(Protocol.MRP)
+    assert "DMAP" == convert.protocol_str(Protocol.DMAP)
+    assert "AirPlay" == convert.protocol_str(Protocol.AirPlay)
+
+
+def test_unknown_protocol_str():
+    assert "Unknown" == convert.protocol_str("invalid")
