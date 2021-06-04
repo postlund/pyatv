71370c99cf9d09be50635766717eeed513fd0076
Pierre Ståhl <pierre.staahl@gmail.com>
2021-06-04 22:18:34 +0200

    raop: Avoid blocking I/O in MiniaudioWrapper
    Relates to #1113

--- pyatv/raop/miniaudio.py
+++ pyatv/raop/miniaudio.py
@@ -5,6 +5,9 @@ provide an interface that is compatible with wave.Wave_read. Using this
 wrapper, any file type supported by miniaudio can be played by the RAOP
 implementation in pyatv.
 """
+import asyncio
+from functools import partial
+
 import miniaudio
 from miniaudio import SampleFormat
 
@@ -30,17 +33,34 @@ class MiniaudioWrapper:
     """
 
     def __init__(
-        self, filename: str, sample_rate: int, channels: int, sample_size: int
+        self,
+        src: miniaudio.DecodedSoundFile,
+        sample_rate: int,
+        channels: int,
+        sample_size: int,
     ) -> None:
         """Initialize a new MiniaudioWrapper instance."""
-        self.src = miniaudio.decode_file(
-            filename,
-            output_format=_int2sf(sample_size),
-            nchannels=channels,
-            sample_rate=sample_rate,
+        self.src: miniaudio.DecodedSoundFile = src
+        self.samples: bytes = self.src.samples.tobytes()
+        self.pos: int = 0
+
+    @classmethod
+    async def open(
+        cls, filename: str, sample_rate: int, channels: int, sample_size: int
+    ) -> "MiniaudioWrapper":
+        """Open an audio file and return an instance of MiniaudioWrapper."""
+        loop = asyncio.get_event_loop()
+        src = await loop.run_in_executor(
+            None,
+            partial(
+                miniaudio.decode_file,
+                filename,
+                output_format=_int2sf(sample_size),
+                nchannels=channels,
+                sample_rate=sample_rate,
+            ),
         )
-        self.samples = self.src.samples.tobytes()
-        self.pos = 0
+        return cls(src, sample_rate, channels, sample_size)
 
     def readframes(self, nframes: int) -> bytes:
         """Read number of frames and advance in stream."""
