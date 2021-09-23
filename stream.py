c9c8cd7719fdd75f2872f6efb157664a8e9a79ae
Pierre Ståhl <pierre.staahl@gmail.com>
2021-09-23 13:10:49 +0200

    raop: Support Disabled pairing requirement

---
+++ stream.py
@@ -0,0 +1,64 @@
+"""Example of streaming a file and printing status updates.
+
+python stream.py 10.0.0.4 file.mp3
+"""
+
+import asyncio
+import sys
+
+import pyatv
+import logging
+from pyatv.interface import Playing, PushListener
+
+LOOP = asyncio.get_event_loop()
+
+
+class PushUpdatePrinter(PushListener):
+    """Print push updates to console."""
+
+    def playstatus_update(self, updater, playstatus: Playing) -> None:
+        """Inform about changes to what is currently playing."""
+        print(30 * "-" + "\n", playstatus)
+
+    def playstatus_error(self, updater, exception: Exception) -> None:
+        """Inform about an error when updating play status."""
+        print("Error:", exception)
+
+
+async def stream_with_push_updates(
+    address: str, filename: str, loop: asyncio.AbstractEventLoop
+):
+    """Find a device and print what is playing."""
+    print("* Discovering device on network...")
+    atvs = await pyatv.scan(loop, hosts=[address], timeout=5)
+
+    if not atvs:
+        print("* Device found", file=sys.stderr)
+        return
+
+    conf = atvs[0]
+
+    print("* Connecting to", conf.address)
+    atv = await pyatv.connect(conf, loop)
+
+    listener = PushUpdatePrinter()
+    atv.push_updater.listener = listener
+    atv.push_updater.start()
+
+    try:
+        print("* Starting to stream", filename)
+        a = asyncio.ensure_future(atv.stream.stream_file(filename))
+        await asyncio.sleep(3)
+        print("stopping")
+        await atv.remote_control.stop()
+        print("waiting")
+        await a
+    except Exception as ex:
+        print("error:", ex)
+    finally:
+        atv.close()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    LOOP.run_until_complete(stream_with_push_updates(sys.argv[1], sys.argv[2], LOOP))
