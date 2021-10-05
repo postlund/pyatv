d1332e86b7c90785252860b45c4a0248e365a9ef
Pierre Ståhl <pierre.staahl@gmail.com>
2021-10-05 22:01:06 +0200

    core: Add generic MessageDispatcher
    Relates to #1302

--- pyatv/core/protocol.py
+++ pyatv/core/protocol.py
@@ -1,7 +1,7 @@
 """Generic functions for protocol logic."""
 import asyncio
 import logging
-from typing import Awaitable, Callable, Optional, TypeVar
+from typing import Awaitable, Callable, Dict, Generic, List, Optional, TypeVar
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -9,6 +9,10 @@ HEARTBEAT_INTERVAL = 30
 HEARTBEAT_RETRIES = 1  # One regular attempt + retries
 
 MessageType = TypeVar("MessageType")
+
+DispatchType = TypeVar("DispatchType")
+DispatchMessage = TypeVar("DispatchMessage")
+DispatchFunc = Callable[[DispatchMessage], Awaitable[None]]
 
 
 async def heartbeater(
@@ -53,3 +57,41 @@ async def heartbeater(
 
     _LOGGER.debug("Stopping heartbeat loop at %d (%s)", count, name)
     finish_func()
+
+
+class MessageDispatcher(Generic[DispatchType, DispatchMessage]):
+    """Dispatch message to listeners based on a type."""
+
+    def __init__(self):
+        """Initialize a new MessageDispatcher instance."""
+        self.__listeners: Dict[DispatchType, List[DispatchFunc]] = {}
+
+    def listen_to(self, dispatch_type: DispatchType, func: DispatchFunc) -> None:
+        """Listen to a specific type of message type."""
+        self.__listeners.setdefault(dispatch_type, []).append(func)
+
+    def dispatch(
+        self, dispatch_type: DispatchType, message: DispatchMessage
+    ) -> List[asyncio.Task]:
+        """Dispatch a message to listeners."""
+
+        async def _call_listener(func):
+            # Make sure to catch any exceptions caused by the listener so we don't get
+            # unfished tasks laying around
+            try:
+                await func
+            except asyncio.CancelledError:
+                pass
+            except Exception:
+                _LOGGER.exception("error during dispatch")
+
+        tasks = []
+        for func in self.__listeners.get(dispatch_type, []):
+            _LOGGER.debug(
+                "Dispatching message with type %s to %s",
+                dispatch_type,
+                func,
+            )
+            tasks.append(asyncio.ensure_future(_call_listener(func(message))))
+
+        return tasks
