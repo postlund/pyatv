3b07514bff9ab1da4c1d2e7450edbdc07dd9ce13
Pierre Ståhl <pierre.staahl@gmail.com>
2021-12-21 14:34:27 +0100

    mrp: Migrate to AbstractPairingHandler
    Relates to #1548

--- pyatv/protocols/mrp/pairing.py
+++ pyatv/protocols/mrp/pairing.py
@@ -2,22 +2,20 @@
 
 import asyncio
 import logging
-from typing import Optional, Union
 
-from pyatv import exceptions
-from pyatv.auth.hap_pairing import parse_credentials
+from pyatv.auth.hap_pairing import HapCredentials
 from pyatv.auth.hap_srp import SRPAuthHandler
-from pyatv.interface import BaseConfig, BaseService, PairingHandler
+from pyatv.core import AbstractPairingHandler
+from pyatv.interface import BaseConfig, BaseService
 from pyatv.protocols.mrp.auth import MrpPairSetupProcedure, MrpPairVerifyProcedure
 from pyatv.protocols.mrp.connection import MrpConnection
 from pyatv.protocols.mrp.protocol import MrpProtocol
-from pyatv.support import error_handler
 from pyatv.support.http import ClientSessionManager
 
 _LOGGER = logging.getLogger(__name__)
 
 
-class MrpPairingHandler(PairingHandler):
+class MrpPairingHandler(AbstractPairingHandler):
     """Base class for API used to pair with an Apple TV."""
 
     def __init__(
@@ -27,62 +25,32 @@ class MrpPairingHandler(PairingHandler):
         session_manager: ClientSessionManager,
         loop: asyncio.AbstractEventLoop,
         **kwargs
-    ):
+    ) -> None:
         """Initialize a new MrpPairingHandler."""
-        super().__init__(session_manager, service)
+        super().__init__(session_manager, service, device_provides_pin=True)
         self.connection = MrpConnection(config.address, self.service.port, loop)
         self.srp = SRPAuthHandler()
         self.protocol = MrpProtocol(self.connection, self.srp, self.service)
         self.pairing_procedure = MrpPairSetupProcedure(self.protocol, self.srp)
-        self.pin_code: Optional[str] = None
-        self._has_paired = False
 
-    async def close(self):
+    async def close(self) -> None:
         """Call to free allocated resources after pairing."""
         self.connection.close()
         await super().close()
 
-    @property
-    def has_paired(self):
-        """If a successful pairing has been performed."""
-        return self._has_paired
-
-    async def begin(self):
+    async def _pair_begin(self) -> None:
         """Start pairing process."""
-        return await error_handler(
-            self.pairing_procedure.start_pairing, exceptions.PairingError
-        )
+        await self.pairing_procedure.start_pairing()
 
-    async def finish(self):
+    async def _pair_finish(self) -> str:
         """Stop pairing process."""
-        if not self.pin_code:
-            raise exceptions.PairingError("no pin given")
-
-        credentials = str(
-            await error_handler(
-                self.pairing_procedure.finish_pairing,
-                exceptions.PairingError,
-                "",  # username required but not used
-                self.pin_code,
-            )
+        credentials: HapCredentials = await self.pairing_procedure.finish_pairing(
+            "", int(self._pin or "0000")
         )
 
         _LOGGER.debug("Verifying credentials %s", credentials)
 
-        verifier = MrpPairVerifyProcedure(
-            self.protocol, self.srp, parse_credentials(credentials)
-        )
-        await error_handler(verifier.verify_credentials, exceptions.PairingError)
+        verifier = MrpPairVerifyProcedure(self.protocol, self.srp, credentials)
+        await verifier.verify_credentials()
 
-        self.service.credentials = credentials
-        self._has_paired = True
-
-    @property
-    def device_provides_pin(self):
-        """Return True if remote device presents PIN code, else False."""
-        return True
-
-    def pin(self, pin: Union[str, int]) -> None:
-        """Pin code used for pairing."""
-        self.pin_code = str(pin).zfill(4)
-        _LOGGER.debug("MRP PIN changed to %s", self.pin_code)
+        return str(credentials)
