23c05d581f9d749f6a26dec1e8b6f6e799efc2ea
J. Nick Koston <nick@koston.org>
2022-01-05 12:53:10 -1000

    Add a basic test

--- tests/core/test_scan.py
+++ tests/core/test_scan.py
@@ -1,9 +1,22 @@
 """Unit tests for scan module."""
 
+import asyncio
 from unittest.mock import patch
 
 import pytest
+from zeroconf import (
+    DNSAddress,
+    DNSPointer,
+    DNSService,
+    DNSText,
+    ServiceListener,
+    Zeroconf,
+    const,
+)
+from zeroconf.asyncio import AsyncServiceBrowser, AsyncZeroconf
 
+from pyatv import scan
+from pyatv.conf import AppleTV
 from pyatv.core.mdns import Response, Service
 from pyatv.core.scan import BaseScanner, get_unique_identifiers
 
@@ -34,3 +47,187 @@ def test_unique_identifiers(unique_id_mock, response):
     assert "id2" == next(identifiers)
     unique_id_mock.assert_called_with("_service2._tcp.local", "service2", {"c": "d"})
     assert not next(identifiers, None)
+
+
+@pytest.mark.asyncio
+async def test_scan_with_zeroconf():
+    aiozc = AsyncZeroconf(interfaces=["127.0.0.1"])
+
+    class DummyListener(ServiceListener):
+        def add_service(self, zeroconf: Zeroconf, type: str, name: str) -> None:
+            pass
+
+        def remove_service(self, zeroconf: Zeroconf, type: str, name: str) -> None:
+            pass
+
+        def update_service(self, zeroconf: Zeroconf, type: str, name: str) -> None:
+            pass
+
+    browser = AsyncServiceBrowser(
+        aiozc.zeroconf,
+        [
+            "_mediaremotetv._tcp.local.",
+            "_companion-link._tcp.local.",
+            "_airport._tcp.local.",
+            "_sleep-proxy._udp.local.",
+            "_touch-able._tcp.local.",
+            "_appletv-v2._tcp.local.",
+            "_hscp._tcp.local.",
+            "_airplay._tcp.local.",
+            "_raop._tcp.local.",
+        ],
+        None,
+        DummyListener(),
+    )
+    a_record = DNSAddress(
+        "Ohana.local.",
+        const._TYPE_A,
+        const._CLASS_IN,
+        const._DNS_HOST_TTL,
+        b"\xc0\xa8k\xb8",
+    )
+
+    sleep_proxy_ptr_record = DNSPointer(
+        "_sleep-proxy._udp.local.",
+        const._TYPE_PTR,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        "70-35-60-63.1 Ohana._sleep-proxy._udp.local.",
+    )
+    sleep_proxy_service_record = DNSService(
+        "70-35-60-63.1 Ohana._sleep-proxy._udp.local.",
+        const._TYPE_SRV,
+        const._CLASS_IN,
+        const._DNS_HOST_TTL,
+        0,
+        0,
+        54942,
+        "Ohana.local.",
+    )
+    sleep_proxy_txt_record = DNSText(
+        "70-35-60-63.1 Ohana._sleep-proxy._udp.local.",
+        const._TYPE_TXT,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        b"",
+    )
+
+    airplay_ptr_record = DNSPointer(
+        "_airplay._tcp.local.",
+        const._TYPE_PTR,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        "Ohana._airplay._tcp.local.",
+    )
+    airplay_service_record = DNSService(
+        "Ohana._airplay._tcp.local.",
+        const._TYPE_SRV,
+        const._CLASS_IN,
+        const._DNS_HOST_TTL,
+        0,
+        0,
+        7000,
+        "Ohana.local.",
+    )
+    airplay_txt_record = DNSText(
+        "Ohana._airplay._tcp.local.",
+        const._TYPE_TXT,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        b"",
+    )
+
+    raop_ptr_record = DNSPointer(
+        "_raop._tcp.local.",
+        const._TYPE_PTR,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        "54E61BF2ED74@Ohana._raop._tcp.local.",
+    )
+    raop_service_record = DNSService(
+        "54E61BF2ED74@Ohana._raop._tcp.local.",
+        const._TYPE_SRV,
+        const._CLASS_IN,
+        const._DNS_HOST_TTL,
+        0,
+        0,
+        7000,
+        "Ohana.local.",
+    )
+    raop_txt_record = DNSText(
+        "Ohana._airplay._tcp.local.",
+        const._TYPE_TXT,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        b"",
+    )
+
+    companion_link_ptr_record = DNSPointer(
+        "_companion-link._tcp.local.",
+        const._TYPE_PTR,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        "Ohana._companion-link._tcp.local.",
+    )
+    companion_link_service_record = DNSService(
+        "Ohana._companion-link._tcp.local.",
+        const._TYPE_SRV,
+        const._CLASS_IN,
+        const._DNS_HOST_TTL,
+        0,
+        0,
+        49152,
+        "Ohana.local.",
+    )
+    companion_link_txt_record = DNSText(
+        "Ohana._companion-link._tcp.local.",
+        const._TYPE_TXT,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        b"",
+    )
+
+    device_info_record = DNSPointer(
+        "_device-info._tcp.local.",
+        const._TYPE_PTR,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        "Ohana._device-info._tcp.local.",
+    )
+    device_info_txt_record = DNSText(
+        "Ohana._device-info._tcp.local.",
+        const._TYPE_TXT,
+        const._CLASS_IN,
+        const._DNS_OTHER_TTL,
+        b"",
+    )
+
+    aiozc.zeroconf.cache.async_add_records(
+        [
+            a_record,
+            sleep_proxy_ptr_record,
+            sleep_proxy_service_record,
+            sleep_proxy_txt_record,
+            airplay_ptr_record,
+            airplay_service_record,
+            airplay_txt_record,
+            raop_ptr_record,
+            raop_service_record,
+            raop_txt_record,
+            companion_link_ptr_record,
+            companion_link_service_record,
+            companion_link_txt_record,
+            device_info_record,
+            device_info_txt_record,
+        ]
+    )
+    results = await scan(asyncio.get_running_loop(), timeout=0.1, async_zeroconf=aiozc)
+    atv: AppleTV = results[0]
+    assert isinstance(atv, AppleTV)
+    assert "_sleep-proxy._udp.local" in atv.properties
+    assert "_airplay._tcp.local" in atv.properties
+    assert "_raop._tcp.local" in atv.properties
+    assert "_companion-link._tcp.local" in atv.properties
+
+    await browser.async_cancel()
+    await aiozc.async_close()
