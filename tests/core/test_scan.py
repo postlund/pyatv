6bd3b3af8106c1a65735de7c13ab729ff7e8a063
J. Nick Koston <nick@koston.org>
2022-01-05 13:09:44 -1000

    there is no ptr for device-info

--- tests/core/test_scan.py
+++ tests/core/test_scan.py
@@ -213,7 +213,7 @@ async def test_scan_with_zeroconf():
             device_info_txt_record,
         ]
     )
-    results = await scan(asyncio.get_running_loop(), timeout=0.1, async_zeroconf=aiozc)
+    results = await scan(asyncio.get_event_loop(), timeout=0.1, async_zeroconf=aiozc)
     atv: AppleTV = results[0]
     assert isinstance(atv, AppleTV)
     assert "_sleep-proxy._udp.local" in atv.properties
