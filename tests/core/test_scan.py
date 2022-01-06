d92758f372141002b275af5e32ebddaff6b56f10
J. Nick Koston <nick@koston.org>
2022-01-05 15:45:12 -1000

    coverage

--- tests/core/test_scan.py
+++ tests/core/test_scan.py
@@ -24,10 +24,213 @@ from pyatv.core.scan import BaseScanner, get_unique_identifiers
 TEST_SERVICE1 = Service("_service1._tcp.local", "service1", None, 0, {"a": "b"})
 TEST_SERVICE2 = Service("_service2._tcp.local", "service2", None, 0, {"c": "d"})
 
+ALL_MDNS_SERVICES = [
+    "_mediaremotetv._tcp.local.",
+    "_companion-link._tcp.local.",
+    "_airport._tcp.local.",
+    "_sleep-proxy._udp.local.",
+    "_touch-able._tcp.local.",
+    "_appletv-v2._tcp.local.",
+    "_hscp._tcp.local.",
+    "_airplay._tcp.local.",
+    "_raop._tcp.local.",
+]
+
+DEVICE_A_RECORD = DNSAddress(
+    "Ohana.local.",
+    const._TYPE_A,
+    const._CLASS_IN,
+    const._DNS_HOST_TTL,
+    b"\xc0\xa8k\xb8",
+)
+SLEEP_PROXY_PTR_RECORD = DNSPointer(
+    "_sleep-proxy._udp.local.",
+    const._TYPE_PTR,
+    const._CLASS_IN,
+    const._DNS_OTHER_TTL,
+    "70-35-60-63.1 Ohana._sleep-proxy._udp.local.",
+)
+SLEEP_PROXY_SRV_RECORD = DNSService(
+    "70-35-60-63.1 Ohana._sleep-proxy._udp.local.",
+    const._TYPE_SRV,
+    const._CLASS_IN,
+    const._DNS_HOST_TTL,
+    0,
+    0,
+    54942,
+    "Ohana.local.",
+)
+SLEEP_PROXY_TXT_RECORD = DNSText(
+    "70-35-60-63.1 Ohana._sleep-proxy._udp.local.",
+    const._TYPE_TXT,
+    const._CLASS_IN,
+    const._DNS_OTHER_TTL,
+    b"",
+)
+AIRPLAY_PTR_RECORD = DNSPointer(
+    "_airplay._tcp.local.",
+    const._TYPE_PTR,
+    const._CLASS_IN,
+    const._DNS_OTHER_TTL,
+    "Ohana._airplay._tcp.local.",
+)
+AIRPLAY_SRV_RECORD = DNSService(
+    "Ohana._airplay._tcp.local.",
+    const._TYPE_SRV,
+    const._CLASS_IN,
+    const._DNS_HOST_TTL,
+    0,
+    0,
+    7000,
+    "Ohana.local.",
+)
+AIRPLAY_TXT_RECORD = DNSText(
+    "Ohana._airplay._tcp.local.",
+    const._TYPE_TXT,
+    const._CLASS_IN,
+    const._DNS_OTHER_TTL,
+    b"",
+)
+RAOP_PTR_RECORD = DNSPointer(
+    "_raop._tcp.local.",
+    const._TYPE_PTR,
+    const._CLASS_IN,
+    const._DNS_OTHER_TTL,
+    "54E61BF2ED74@Ohana._raop._tcp.local.",
+)
+RAOP_SERVICE_RECORD = DNSService(
+    "54E61BF2ED74@Ohana._raop._tcp.local.",
+    const._TYPE_SRV,
+    const._CLASS_IN,
+    const._DNS_HOST_TTL,
+    0,
+    0,
+    7000,
+    "Ohana.local.",
+)
+RAOP_TXT_RECORD = DNSText(
+    "Ohana._airplay._tcp.local.",
+    const._TYPE_TXT,
+    const._CLASS_IN,
+    const._DNS_OTHER_TTL,
+    b"",
+)
+COMPANION_LINK_PTR_RECORD = DNSPointer(
+    "_companion-link._tcp.local.",
+    const._TYPE_PTR,
+    const._CLASS_IN,
+    const._DNS_OTHER_TTL,
+    "Ohana._companion-link._tcp.local.",
+)
+COMPANION_LINK_SRV_RECORD = DNSService(
+    "Ohana._companion-link._tcp.local.",
+    const._TYPE_SRV,
+    const._CLASS_IN,
+    const._DNS_HOST_TTL,
+    0,
+    0,
+    49152,
+    "Ohana.local.",
+)
+COMPANION_LINK_TXT_RECORD = DNSText(
+    "Ohana._companion-link._tcp.local.",
+    const._TYPE_TXT,
+    const._CLASS_IN,
+    const._DNS_OTHER_TTL,
+    b"",
+)
+DEVICE_INFO_TEXT_RECORD = DNSText(
+    "Ohana._device-info._tcp.local.",
+    const._TYPE_TXT,
+    const._CLASS_IN,
+    const._DNS_OTHER_TTL,
+    b"\x0Cmodel=J305AP",
+)
+SLEEP_PROXY_RECORDS = [
+    SLEEP_PROXY_PTR_RECORD,
+    SLEEP_PROXY_SRV_RECORD,
+    SLEEP_PROXY_TXT_RECORD,
+]
+AIRPLAY_RECORDS = [
+    AIRPLAY_PTR_RECORD,
+    AIRPLAY_SRV_RECORD,
+    AIRPLAY_TXT_RECORD,
+]
+RAOP_RECORDS = [
+    RAOP_PTR_RECORD,
+    RAOP_SERVICE_RECORD,
+    RAOP_TXT_RECORD,
+]
+COMPANION_LINK_RECORDS = [
+    COMPANION_LINK_PTR_RECORD,
+    COMPANION_LINK_SRV_RECORD,
+    COMPANION_LINK_TXT_RECORD,
+]
+
+
+COMPLETE_RECORD_SET_WITH_DEVICE_INFO = [
+    DEVICE_A_RECORD,
+    *SLEEP_PROXY_RECORDS,
+    *AIRPLAY_RECORDS,
+    *RAOP_RECORDS,
+    *COMPANION_LINK_RECORDS,
+    DEVICE_INFO_TEXT_RECORD,
+]
+COMPLETE_RECORD_SET = [
+    DEVICE_A_RECORD,
+    *SLEEP_PROXY_RECORDS,
+    *AIRPLAY_RECORDS,
+    *RAOP_RECORDS,
+    *COMPANION_LINK_RECORDS,
+]
+RECORD_SET_WITH_DEVICE_INFO_MISSING_COMPANION_LINK = [
+    DEVICE_A_RECORD,
+    *SLEEP_PROXY_RECORDS,
+    *AIRPLAY_RECORDS,
+    *RAOP_RECORDS,
+    DEVICE_INFO_TEXT_RECORD,
+]
+PARTIAL_RECORD_SET = [
+    DEVICE_A_RECORD,
+    *SLEEP_PROXY_RECORDS,
+    *AIRPLAY_RECORDS,
+]
+
+
+from typing import List, Tuple
+
+from zeroconf import DNSRecord
+
+
+async def _async_create_zc_with_cache(
+    records: List[DNSRecord],
+) -> Tuple[AsyncZeroconf, AsyncServiceBrowser]:
+    aiozc = AsyncZeroconf(interfaces=["127.0.0.1"])
+    browser = AsyncServiceBrowser(
+        aiozc.zeroconf,
+        ALL_MDNS_SERVICES,
+        None,
+        DummyListener(),
+    )
+    aiozc.zeroconf.cache.async_add_records(records)
+    await aiozc.zeroconf.async_wait_for_start()
+    return aiozc, browser
+
 
 @pytest.fixture
 def response():
     yield Response([], False, None)
+
+
+class DummyListener(ServiceListener):
+    def add_service(self, zeroconf: Zeroconf, type: str, name: str) -> None:
+        pass
+
+    def remove_service(self, zeroconf: Zeroconf, type: str, name: str) -> None:
+        pass
+
+    def update_service(self, zeroconf: Zeroconf, type: str, name: str) -> None:
+        pass
 
 
 def test_unique_identifier_empty(response):
@@ -51,180 +254,13 @@ def test_unique_identifiers(unique_id_mock, response):
 
 
 @pytest.mark.asyncio
-async def test_scan_with_zeroconf():
+async def test_scan_with_zeroconf_complete_and_device_info():
     if not hasattr(asyncio, "get_running_loop"):
         # until python 3.6 is removed, its EOL anyways
         asyncio.get_running_loop = asyncio.get_event_loop
-    aiozc = AsyncZeroconf(interfaces=["127.0.0.1"])
-
-    class DummyListener(ServiceListener):
-        def add_service(self, zeroconf: Zeroconf, type: str, name: str) -> None:
-            pass
-
-        def remove_service(self, zeroconf: Zeroconf, type: str, name: str) -> None:
-            pass
-
-        def update_service(self, zeroconf: Zeroconf, type: str, name: str) -> None:
-            pass
-
-    browser = AsyncServiceBrowser(
-        aiozc.zeroconf,
-        [
-            "_mediaremotetv._tcp.local.",
-            "_companion-link._tcp.local.",
-            "_airport._tcp.local.",
-            "_sleep-proxy._udp.local.",
-            "_touch-able._tcp.local.",
-            "_appletv-v2._tcp.local.",
-            "_hscp._tcp.local.",
-            "_airplay._tcp.local.",
-            "_raop._tcp.local.",
-        ],
-        None,
-        DummyListener(),
+    aiozc, browser = await _async_create_zc_with_cache(
+        COMPLETE_RECORD_SET_WITH_DEVICE_INFO
     )
-    a_record = DNSAddress(
-        "Ohana.local.",
-        const._TYPE_A,
-        const._CLASS_IN,
-        const._DNS_HOST_TTL,
-        b"\xc0\xa8k\xb8",
-    )
-
-    sleep_proxy_ptr_record = DNSPointer(
-        "_sleep-proxy._udp.local.",
-        const._TYPE_PTR,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        "70-35-60-63.1 Ohana._sleep-proxy._udp.local.",
-    )
-    sleep_proxy_service_record = DNSService(
-        "70-35-60-63.1 Ohana._sleep-proxy._udp.local.",
-        const._TYPE_SRV,
-        const._CLASS_IN,
-        const._DNS_HOST_TTL,
-        0,
-        0,
-        54942,
-        "Ohana.local.",
-    )
-    sleep_proxy_txt_record = DNSText(
-        "70-35-60-63.1 Ohana._sleep-proxy._udp.local.",
-        const._TYPE_TXT,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        b"",
-    )
-
-    airplay_ptr_record = DNSPointer(
-        "_airplay._tcp.local.",
-        const._TYPE_PTR,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        "Ohana._airplay._tcp.local.",
-    )
-    airplay_service_record = DNSService(
-        "Ohana._airplay._tcp.local.",
-        const._TYPE_SRV,
-        const._CLASS_IN,
-        const._DNS_HOST_TTL,
-        0,
-        0,
-        7000,
-        "Ohana.local.",
-    )
-    airplay_txt_record = DNSText(
-        "Ohana._airplay._tcp.local.",
-        const._TYPE_TXT,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        b"",
-    )
-
-    raop_ptr_record = DNSPointer(
-        "_raop._tcp.local.",
-        const._TYPE_PTR,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        "54E61BF2ED74@Ohana._raop._tcp.local.",
-    )
-    raop_service_record = DNSService(
-        "54E61BF2ED74@Ohana._raop._tcp.local.",
-        const._TYPE_SRV,
-        const._CLASS_IN,
-        const._DNS_HOST_TTL,
-        0,
-        0,
-        7000,
-        "Ohana.local.",
-    )
-    raop_txt_record = DNSText(
-        "Ohana._airplay._tcp.local.",
-        const._TYPE_TXT,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        b"",
-    )
-
-    companion_link_ptr_record = DNSPointer(
-        "_companion-link._tcp.local.",
-        const._TYPE_PTR,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        "Ohana._companion-link._tcp.local.",
-    )
-    companion_link_service_record = DNSService(
-        "Ohana._companion-link._tcp.local.",
-        const._TYPE_SRV,
-        const._CLASS_IN,
-        const._DNS_HOST_TTL,
-        0,
-        0,
-        49152,
-        "Ohana.local.",
-    )
-    companion_link_txt_record = DNSText(
-        "Ohana._companion-link._tcp.local.",
-        const._TYPE_TXT,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        b"",
-    )
-    device_info_ptr_record = DNSPointer(
-        "_device-info._tcp.local.",
-        const._TYPE_PTR,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        "Ohana._device-info._tcp.local.",
-    )
-    device_info_txt_record = DNSText(
-        "Ohana._device-info._tcp.local.",
-        const._TYPE_TXT,
-        const._CLASS_IN,
-        const._DNS_OTHER_TTL,
-        b"\x0Cmodel=J305AP",
-    )
-
-    aiozc.zeroconf.cache.async_add_records(
-        [
-            a_record,
-            sleep_proxy_ptr_record,
-            sleep_proxy_service_record,
-            sleep_proxy_txt_record,
-            airplay_ptr_record,
-            airplay_service_record,
-            airplay_txt_record,
-            raop_ptr_record,
-            raop_service_record,
-            raop_txt_record,
-            companion_link_ptr_record,
-            companion_link_service_record,
-            companion_link_txt_record,
-            device_info_ptr_record,
-            device_info_txt_record,
-        ]
-    )
-    await aiozc.zeroconf.async_wait_for_start()
     results = await scan(asyncio.get_event_loop(), timeout=0, async_zeroconf=aiozc)
     atv: AppleTV = results[0]
     assert isinstance(atv, AppleTV)
@@ -232,6 +268,100 @@ async def test_scan_with_zeroconf():
     assert "_airplay._tcp.local" in atv.properties
     assert "_raop._tcp.local" in atv.properties
     assert "_companion-link._tcp.local" in atv.properties
+    assert atv.deep_sleep is False
+    assert atv.device_info.model == DeviceModel.AppleTV4KGen2
+    await browser.async_cancel()
+    await aiozc.async_close()
+
+
+@pytest.mark.asyncio
+async def test_scan_with_zeroconf_complete_and_device_info_specific_host_matching():
+    if not hasattr(asyncio, "get_running_loop"):
+        # until python 3.6 is removed, its EOL anyways
+        asyncio.get_running_loop = asyncio.get_event_loop
+    aiozc, browser = await _async_create_zc_with_cache(
+        COMPLETE_RECORD_SET_WITH_DEVICE_INFO
+    )
+    results = await scan(
+        asyncio.get_event_loop(),
+        hosts=["192.168.107.184"],
+        timeout=0,
+        async_zeroconf=aiozc,
+    )
+    atv: AppleTV = results[0]
+    assert isinstance(atv, AppleTV)
+    assert "_sleep-proxy._udp.local" in atv.properties
+    assert "_airplay._tcp.local" in atv.properties
+    assert "_raop._tcp.local" in atv.properties
+    assert "_companion-link._tcp.local" in atv.properties
+    assert atv.deep_sleep is False
+    assert atv.device_info.model == DeviceModel.AppleTV4KGen2
+    await browser.async_cancel()
+    await aiozc.async_close()
+
+
+@pytest.mark.asyncio
+async def test_scan_with_zeroconf_complete_and_device_info_specific_host_not_matching():
+    if not hasattr(asyncio, "get_running_loop"):
+        # until python 3.6 is removed, its EOL anyways
+        asyncio.get_running_loop = asyncio.get_event_loop
+    aiozc, browser = await _async_create_zc_with_cache(
+        COMPLETE_RECORD_SET_WITH_DEVICE_INFO
+    )
+    results = await scan(
+        asyncio.get_event_loop(), hosts=["192.168.1.1"], timeout=0, async_zeroconf=aiozc
+    )
+    assert len(results) == 0
+    await browser.async_cancel()
+    await aiozc.async_close()
+
+
+@pytest.mark.asyncio
+async def test_scan_with_zeroconf_complete():
+    if not hasattr(asyncio, "get_running_loop"):
+        # until python 3.6 is removed, its EOL anyways
+        asyncio.get_running_loop = asyncio.get_event_loop
+    aiozc, browser = await _async_create_zc_with_cache(COMPLETE_RECORD_SET)
+    results = await scan(asyncio.get_event_loop(), timeout=0, async_zeroconf=aiozc)
+    atv: AppleTV = results[0]
+    assert isinstance(atv, AppleTV)
+    assert "_sleep-proxy._udp.local" in atv.properties
+    assert "_airplay._tcp.local" in atv.properties
+    assert "_raop._tcp.local" in atv.properties
+    assert "_companion-link._tcp.local" in atv.properties
+    assert atv.deep_sleep is False
+    assert atv.device_info.model == DeviceModel.Unknown
+    await browser.async_cancel()
+    await aiozc.async_close()
+
+
+@pytest.mark.asyncio
+async def test_scan_with_zeroconf_partial():
+    if not hasattr(asyncio, "get_running_loop"):
+        # until python 3.6 is removed, its EOL anyways
+        asyncio.get_running_loop = asyncio.get_event_loop
+    aiozc, browser = await _async_create_zc_with_cache(PARTIAL_RECORD_SET)
+    results = await scan(asyncio.get_event_loop(), timeout=0, async_zeroconf=aiozc)
+    assert len(results) == 0
+    await browser.async_cancel()
+    await aiozc.async_close()
+
+
+@pytest.mark.asyncio
+async def test_scan_with_zeroconf_missing_companion_link_only():
+    if not hasattr(asyncio, "get_running_loop"):
+        # until python 3.6 is removed, its EOL anyways
+        asyncio.get_running_loop = asyncio.get_event_loop
+    aiozc, browser = await _async_create_zc_with_cache(
+        RECORD_SET_WITH_DEVICE_INFO_MISSING_COMPANION_LINK
+    )
+    results = await scan(asyncio.get_event_loop(), timeout=0, async_zeroconf=aiozc)
+    atv: AppleTV = results[0]
+    assert isinstance(atv, AppleTV)
+    assert "_sleep-proxy._udp.local" in atv.properties
+    assert "_airplay._tcp.local" in atv.properties
+    assert "_raop._tcp.local" in atv.properties
+    assert "_companion-link._tcp.local" not in atv.properties
     assert atv.deep_sleep is False
     assert atv.device_info.model == DeviceModel.AppleTV4KGen2
     await browser.async_cancel()
