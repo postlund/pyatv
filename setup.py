09d66b453f9d108554f691b7ccc865537e95545a
Pierre Ståhl <pierre.sigma@renzgroup.com>
2023-04-18 20:04:40 +0200

    gha: Run tests with python 3.11

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries",
         "Topic :: Home Automation",
         "Typing :: Typed",
