dbb1a887c9fe8d15c490720b86fe36f18655b996
Pierre Ståhl <pierre.staahl@gmail.com>
2026-05-25 21:22:39 +0200

    Revert "Reported modification to fakedevice for testings"
    This reverts commit 5da5903109653c32c318d89ad09dfd1a1f6615e9.

--- tests/fake_device/companion.py
+++ tests/fake_device/companion.py
@@ -484,9 +484,6 @@ class FakeCompanionService(CompanionServerAuth, asyncio.Protocol):
         else:
             self.send_error(message, "Invalid SID")
 
-    def handle_tvrcsessionstart(self, message):
-        self.send_response(message, message.get("_c", {}))
-
     def handle__systeminfo(self, message):
         self.send_response(message, {})
 
