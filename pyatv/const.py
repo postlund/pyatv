d88abc960e188d3cd2498d19117e75d7711d8600
Pierre Ståhl <pierre.staahl@gmail.com>
2026-06-19 22:48:33 +0200

    Release 0.18.0

--- pyatv/const.py
+++ pyatv/const.py
@@ -5,7 +5,7 @@
 from enum import Enum
 
 MAJOR_VERSION = "0"
-MINOR_VERSION = "17"
+MINOR_VERSION = "18"
 PATCH_VERSION = "0"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
